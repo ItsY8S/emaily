c7e7cd0d381fabc2293a2eaf510a4d6f49fc8811
ItsY8S <gri.y8s@gmail.com>
2019-02-08 14:58:54 -0500

    Added logout functionality

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -14,7 +14,12 @@ module.exports = app => {
 
   app.get('/auth/twitter/callback', passport.authenticate('twitter'))
 
-  app.get('/api/current_user', (req, res, next) => {
+  app.get('/api/logout', (req, res) => {
+    req.logout()
+    res.send(req.user)
+  })
+
+  app.get('/api/current_user', (req, res) => {
     res.send(req.user)
   })
 }
